dd6713ce65a6683c88c3764b23075b5a8c6f0279
Hritwik <Hritwiksinghal@outlook.in>
2021-09-12 13:06:35 +0530

    add comments, remove redundent functions

--- src/sony_up.py
+++ src/sony_up.py
@@ -22,28 +22,57 @@ def get_name_no_ext(filename):
     return str(filename).strip()
 
 
-class parallel_downloader:
+class downloader:
 
     def __init__(self):
         self._save_dir: str = ''
         self._start_vid_no: int = -1
 
     def __create_search_query(self, filename: str):
+        """
+        Create search Query from filename
+        :param filename: str
+        :return: str
+        """
         ep_name = get_name_no_ext(filename)
 
         return 'Taarak Mehta Ka Ooltah Chashmah ' + ep_name + ' sonyliv'
 
+    def __check_url(self, url: str):
+        """
+        checks if the url is correct for that episode
+        :param url:
+        :return: bool
+        """
+        if url == '':
+            return False
+
+        # todo : parse url and check episode number
+
+        return True
+
     def __get_best_result(self, results: dict):
+        """
+        from all the results, get the best match
+        :param results: dict: dict (from Searx) which contains data in json format
+        :return: str: url of the best match result
+        """
         for result in results:
             if result['engines'][0] == 'google' and result['parsed_url'][1] == 'www.sonyliv.com':
                 url = result['url']
-                # print(url)
-                if url != '':
+                if self.__check_url(url):
+                    # print(url)
                     return url
 
         return ''
 
     def __create_folders(self, filename: str, old_save_dir):
+        """
+        Will create a folder for each filename at old_save_dir
+        :param filename: str:  Name of file (ep number)
+        :param old_save_dir: where folder is to be created
+        :return:
+        """
         filename = get_name_no_ext(filename)
         download_dir = os.path.expanduser(old_save_dir + '/' + filename)
 
@@ -51,7 +80,12 @@ class parallel_downloader:
             os.mkdir(download_dir)
         self._save_dir = download_dir
 
-    def __download(self, url):
+    def __download(self, url: str):
+        """
+        Start Download using YTDL class and save it save_dir
+        :param url: url to be downloaded
+        :return:
+        """
         my_ytdl = ytdl_downloader.YTDL_Downloader()
         os.chdir(self._save_dir)
         print("chdir to ", self._save_dir)
@@ -59,7 +93,15 @@ class parallel_downloader:
         my_ytdl.set_download_url(url)
         my_ytdl.start()
 
-    def __rename_move(self, file, global_save_dir):
+    def __rename_move(self, file: tuple, global_save_dir: str):
+        """
+        check if file has been downloaded, then rename and move it accordingly
+        :param file: tuple:
+        :param global_save_dir: str
+        :return:
+        """
+
+        # todo: check if file has been downloaded
         os.chdir(self._save_dir)
 
         old_name: str = os.listdir(self._save_dir)[0]
@@ -73,7 +115,13 @@ class parallel_downloader:
 
         os.rename(old_name_full, new_name)
 
-    def __update_tuple(self, tuple_name, pos):
+    def __update_tuple(self, tuple_name: tuple, pos: int):
+        """
+        Update the tuple's flag at position 'pos' to True (when download has completed)
+        :param tuple_name: tuple: tuple to be changed
+        :param pos: int: position at which change occurs
+        :return:
+        """
         y = list(tuple_name)
         y[pos] = True
         tuple_name = tuple(y)
@@ -85,19 +133,18 @@ class parallel_downloader:
 
         for i in range(start_vid_no, start_vid_no + 4):
             print()
-
-            # create save directory for each file
             file = files[i]
 
             # If file has been downloaded, skip this
             if file[2]:
                 continue
 
-            self.__create_folders(file[1], global_save_dir)
+            # create save directory for each file, Not needed after adding episode_number in YTDL 'outtmpl'
+            # self.__create_folders(file[1], global_save_dir)
 
             # Get results for each file
             search_query: str = self.__create_search_query(file[1])
-            print(search_query)
+            # print(search_query)
             results: dict = searx.SearX().get_results_json(search_query=search_query)['results']
             url = self.__get_best_result(results)
 
@@ -110,6 +157,7 @@ class parallel_downloader:
                     file = self.__update_tuple(file, 2)
 
                     # Download has finished, now rename file and move it to common location
+                    # Not needed  after adding episode_number in YTDL 'outtmpl'
                     # self.__rename_move(file, global_save_dir)
 
                 except:
@@ -123,10 +171,12 @@ class Upgrade_SL:
         self.files_dir = '/Videos'
         self.exclude_dirs = {
             'ABC': '1',
-            'XYZ': '0'
+            'XYZ': '0',
+            'AAA': '1'
         }
+        
         self._save_dir: str = save_dir
-        self._files = []
+        self._files: list = []
 
         self.__init_func()
 
@@ -150,15 +200,26 @@ class Upgrade_SL:
             self._save_dir = download_dir
         print("Save Dir: ", self._save_dir)
 
+    def __update_file_list(self):
+        """
+        Add an extra bool field to each tuple of '_files' which is true when the download of that file in complete
+        :return:
+        """
+        for i in range(len(self._files)):
+            file = self._files[i]
+            y = list(file)
+            y.append(False)
+            self._files[i] = tuple(y)
+
     def __pp_dict(self, json_dict: dict):
         print(json.dumps(json_dict, indent=4))
 
     def __parallel(self):
         # creating processes
-        p1 = multiprocessing.Process(target=parallel_downloader().start, args=(0, self._files, self._save_dir,))
-        p2 = multiprocessing.Process(target=parallel_downloader().start, args=(4, self._files, self._save_dir,))
-        p3 = multiprocessing.Process(target=parallel_downloader().start, args=(8, self._files, self._save_dir,))
-        p4 = multiprocessing.Process(target=parallel_downloader().start, args=(12, self._files, self._save_dir,))
+        p1 = multiprocessing.Process(target=downloader().start, args=(0, self._files, self._save_dir,))
+        p2 = multiprocessing.Process(target=downloader().start, args=(4, self._files, self._save_dir,))
+        p3 = multiprocessing.Process(target=downloader().start, args=(8, self._files, self._save_dir,))
+        p4 = multiprocessing.Process(target=downloader().start, args=(12, self._files, self._save_dir,))
 
         # starting process
         p1.start()
@@ -179,7 +240,10 @@ class Upgrade_SL:
         # Get files list
         my_crawler = file_crawler.File_Crawler(file_dir=self.files_dir, exclude_dirs=self.exclude_dirs,
                                                recursive_crawl_flag=True)
-        self._files = my_crawler.get_files()
+        self._files: list = my_crawler.get_files()
+
+        # Add boolean flag to files which will be true when file is downloaded.
+        self.__update_file_list()
 
         """
         self._files : list : List of files in that folder,
