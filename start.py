1d89a9b973d341e494ef889868a5395bc924994f
Hritwik <Hritwiksinghal@outlook.in>
2021-09-13 23:34:04 +0530

    update start file

--- start.py
+++ start.py
@@ -1,5 +1,15 @@
-from src.sony_up import Upgrade_SL
+from src.sony_downloader import Downloader
 
 if __name__ == '__main__':
-    upgrade = Upgrade_SL()
-    upgrade.start()
+    files_dir = '/run/media/'
+    exclude_dirs = {
+        'AAA': '1',
+        'AEE': '0',
+        'OEOE': '1',
+        '>U>U>': '1'
+    }
+    search_prefix = 'AAA'
+    search_suffix = 'BBB'
+
+    my_downloader = Downloader(files_dir=files_dir, exclude_dirs=exclude_dirs)
+    my_downloader.start(search_prefix=search_prefix, search_suffix=search_suffix)
